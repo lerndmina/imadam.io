092fb54c2a0574430b28e5e2c4090a19ed2e4c45
Adam <wild@lerndmina.dev>
2023-02-03 22:24:10 +0000

    Migrate most pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), tailwind(), react()]
 });
---
+++ src/data/navdata.ts
@@ -0,0 +1,18 @@
+export const navData = [
+    {
+        name: "Home",
+        path: "/"
+    },
+    {
+        name: "Blog",
+        path: "/blog/"
+    },
+    {
+        name: "My-CV",
+        path: "/cv/"
+    },
+    {
+        name: "Contact Me",
+        path: "/contact/"
+    },
+]
