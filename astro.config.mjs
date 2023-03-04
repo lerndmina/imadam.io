b4b6ad6e0a787e02902167c493f43c96d15200a1
Adam <wild@lerndmina.dev>
2023-03-04 17:08:27 +0000

    Build & Remove CF

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import react from "@astrojs/react";
@@ -11,15 +11,13 @@ import react from "@astrojs/react";
 import image from "@astrojs/image";
 
 // https://astro.build/config
-
-// https://astro.build/config
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), react(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  })],
-  output: "server",
-  adapter: cloudflare()
+  integrations: [
+    mdx(),
+    tailwind(),
+    react(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });
