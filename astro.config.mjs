2ae327603a9aa7ef6a4b265c5f2d04adc312b351
Adam <wild@lerndmina.dev>
2023-08-17 09:57:55 +0100

    upgraded mdx to fix issues with collection

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
 
 // https://astro.build/config
 import image from "@astrojs/image";
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
