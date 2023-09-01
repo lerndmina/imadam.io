ce0d9ca5ea891996a2bc08c971f16de21372a69b
Adam <wild@lerndmina.dev>
2023-09-01 13:48:55 +0100

    Optimised more images

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,25 +10,27 @@ interface ImageMetadata {
 
 
 
-import defaultImage from "../assets/images/posts/hq-background.png";
 import { any } from 'astro/zod';
 
 const blog = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    date: z.date(),
-    author: z.enum(["Adam B", "Adam", "Wild"]),
-    image: z.object({
-      src: z.string(),
-      width: z.number().optional(),
-      height: z.number().optional(),
-      format: z.string().optional(),
-    }).optional().default({ src: defaultImage.src}),
-    imageTitle: z.string().optional().nullable(),
-    description: z.string(),
-    draft: z.boolean().default(false),
-    category: z.enum(["Coding", "Personal", "General", "University"]),
-  }),
+  schema: ({ image }) => 
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      author: z.enum(["Adam B", "Adam", "Wild"]),
+      // image: z.object({
+      //   src: z.string(),
+      //   width: z.number().optional(),
+      //   height: z.number().optional(),
+      //   format: z.string().optional(),
+      // }).optional().default({ src: defaultImage.src}),
+      image: image().optional().default("../../assets/images/posts/hq-background.png"), 
+      // Default path is reletive from blog posts not from here
+      imageTitle: z.string().optional().nullable(),
+      description: z.string(),
+      draft: z.boolean().default(false),
+      category: z.enum(["Coding", "Personal", "General", "University"]),
+    }),
 });
 
 export const collections = { blog };
