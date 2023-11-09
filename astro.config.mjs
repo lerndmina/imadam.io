8f44bf337954315865ebf520f6b5eea86d31a7e6
Adam <wild@lerndmina.dev>
2023-11-09 18:02:09 +0000

    Build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   // experimental: {
   //   viewTransitions: true,
   // },
-  integrations: [mdx(), tailwind()],
+  site: "https://imadam.io",
+  integrations: [mdx(), tailwind(), sitemap()],
 });
