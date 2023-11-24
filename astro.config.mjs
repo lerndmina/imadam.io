3a0617a0564f8712b55de64e5cf2cf46e2e58358
Adam <wild@lerndmina.dev>
2023-11-24 23:59:51 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
-
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
-import compress from "astro-compress";
-
 // https://astro.build/config
 export default defineConfig({
   // experimental: {
   //   viewTransitions: true,
   // },
   site: "https://imadam.io",
-  integrations: [mdx(), tailwind(), sitemap(), compress()]
+  integrations: [mdx(), tailwind(), sitemap()],
 });
