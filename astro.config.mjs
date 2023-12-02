1489810a9b35e32022d4be647d50b004e4672016
Adam <wild@lerndmina.dev>
2023-12-02 00:12:09 +0000

    Added vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
-
-// https://astro.build/config
+import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercelServerless(),
   // experimental: {
   //   viewTransitions: true,
   // },
