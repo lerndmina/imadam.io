ffe1ca45834267c2b16d4b025e5344f3c5f348dd
Adam <wild@lerndmina.dev>
2024-06-08 13:43:54 +0100

    Added Vercel check to allow building on vercel and docker

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,25 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 import icon from "astro-icon";
 
+let adapter = node({
+  mode: "standalone",
+});
+
+if (process.env.USING_VERCEL) {
+  adapter = vercel({
+    imageService: true,
+    devImageService: "sharp",
+    speedInsights: {
+      enabled: true,
+    },
+    webAnalytics: {
+      enabled: true,
+    },
+  });
+}
 // https://astro.build/config
 export default defineConfig({
   image: {
@@ -13,12 +30,7 @@ export default defineConfig({
       },
     ],
   },
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
-  // experimental: {
-  //   viewTransitions: true,
-  // },
   integrations: [mdx(), tailwind(), icon()],
+  output: "server",
+  server: serverConfig,
 });
