6d40d636a96550f50bc1a184476eb190a43c014e
Adam <wild@lerndmina.dev>
2024-08-16 18:28:04 +0100

    Refactor Steam link API endpoint to use new redirect URL format

--- src/pages/api/steamlink/[steamlink].ts
+++ src/pages/api/steamlink/[steamlink].ts
@@ -3,6 +3,5 @@ import { R } from "../../../../dist/server/chunks/astro_eDWLrh97.mjs";
 export async function GET({ params, request, redirect }) {
   const steamlink = params.steamlink;
   console.debug(steamlink);
-  if (!steamlink.startsWith("steam://")) return new Response("Invalid steam link", { status: 400 });
-  return redirect(steamlink);
+  return redirect(`steam://connect/${steamlink}`);
 }
