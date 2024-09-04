9a2b15a8fe7052030e50fbd85ae727f33fb635f8
Adam <wild@lerndmina.dev>
2024-09-04 17:11:23 +0100

    refactor: Remove unused import in Steam link API endpoint

--- src/pages/api/steamlink/[...steamlink].ts
+++ src/pages/api/steamlink/[...steamlink].ts
@@ -1,5 +1,3 @@
-import { R } from "../../../../dist/server/chunks/astro_eDWLrh97.mjs";
-
 export async function GET({ params, request, redirect }) {
   const steamlink = params.steamlink;
   console.debug(steamlink);
